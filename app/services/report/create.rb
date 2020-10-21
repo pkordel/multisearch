7e107656a7a3eb57d930ff51d06a2094042a1032
Peter Kordel <pkordel@gmail.com>
2020-10-22 03:58:14 +0700

    Create a report and delegate to search result creation service

---
+++ app/services/report/create.rb
@@ -0,0 +1,24 @@
+class Report < ApplicationRecord
+  class Create
+    attr_reader :report
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def initialize(report_params)
+      @report = Report.new(report_params)
+    end
+
+    def call
+      process_keywords if report.save
+      report
+    end
+
+    private
+
+    def process_keywords
+      SearchResult::Create.call(report: report)
+    end
+  end
+end
